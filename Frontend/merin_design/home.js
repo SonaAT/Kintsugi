142e865be0e8fe120a40c4eb1c2430379ac6a388
Maria-Tresa-Binu <mariatresabinu@gmail.com>
2025-03-04 10:44:33 +0530

    Frontend

---
+++ Frontend/merin_design/home.js
@@ -0,0 +1,5 @@
+// JavaScript for breathing animation
+document.getElementById('play-animation').addEventListener('click', () => {
+  const container = document.getElementById('animation-container');
+  container.style.display = container.style.display === 'block' ? 'none' : 'block';
+});
