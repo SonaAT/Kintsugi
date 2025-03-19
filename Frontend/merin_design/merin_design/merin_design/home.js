a9cec37ce8a54d2a2195deb7ea0d00dc5663c877
Merin Chinnu Varghese <merinirine2003@gmail.com>
2025-03-19 22:55:34 +0530

    Added new file

--- Frontend/merin_design/merin_design/merin_design/home.js
+++
@@ -1,5 +0,0 @@
-// JavaScript for breathing animation
-document.getElementById('play-animation').addEventListener('click', () => {
-  const container = document.getElementById('animation-container');
-  container.style.display = container.style.display === 'block' ? 'none' : 'block';
-});
