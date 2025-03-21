c99420727ab7cb391f95ff610e495d17d0c40ef0
32.sona.antho@gmail.com <32.sona.antho@gmail.com>
2025-03-22 00:43:36 +0530

    django app files updated to handle login, signup and logout

--- iris/chatbot/models.py
+++ iris/chatbot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class PanicAttackEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trigger = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class DailyMoodEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mood = models.CharField(max_length=255)  # Can store mood as a text like "Happy", "Anxious", etc.
+    timestamp = models.DateTimeField(auto_now_add=True)
---
+++ iris/chatbot/static/home.js
@@ -0,0 +1,5 @@
+// Logout function
+function logout() {
+    // Redirect to login page
+    window.location.href = "login.html";
+}
